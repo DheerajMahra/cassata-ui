2a31dcfc110a7a38db37f97fcf83c4a13b498768
DheerajMahra <dheerajmahra873@gmail.com>
2024-11-04 16:47:17 +0530

    chore(storybook): read storybook docs from /docs directory as well

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,11 @@ import type { StorybookConfig } from "@storybook/react-vite";
 import path from "path";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../docs/**/*.mdx",
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
